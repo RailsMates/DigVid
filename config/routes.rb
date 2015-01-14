51673ad5d714484f334c60a2f0111d6681ab69b0
Grzegorz Pozoga <g.pozoga@gmail.com>
2015-01-14 19:02:52 +0100

    Routes and seeds created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'clips#index'
   devise_for :users
+
+  get '/waiting' => "clips#pick"
+  get '/search' => "clips#search"
+  get '/waiting' => "clips#pick"
+  get '/my_account' => "users#show"
+  get '/user/:username' => "users#show"
+  get '/add_clip' => "clips#create"
+  get '/add_category' => "categories#create"
+  get '/top/:period' => "clips#top"
+  get '/top/:category' => "clips#cat_top"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,23 @@
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Clip.destroy_all
+
+20.times do
+
+	 User.create( username: Faker::Internet.user_name , name: Faker::Internet.user_name, email: Faker::Internet.email, password: Faker::Internet.password(8), gender: "male" )
+
+end
+
+ admin = User.create( username: "szefowa" , name: "szefowa" , email: "szefowa@wp.pl" , password: "szefowa" , gender: "female" )
+
+p "seeds created"
+
+
+Clip.create(URL: "https://www.youtube.com/watch?v=ZCx0X2YSd1M")
+
+Clip.create(URL: "https://www.youtube.com/watch?v=lp-EO5I60KA")
+
+Clip.create(URL: "https://www.youtube.com/watch?v=spxg0O05kiA")
+
+Clip.create(URL: "https://www.youtube.com/watch?v=VjHMDlAPMUw")
+
+p "clips created"
